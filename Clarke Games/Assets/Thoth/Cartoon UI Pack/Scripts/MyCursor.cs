545278f27a543853a60eaf790ba0dadc9cca1171
jamie <jamies64gaming@gmail.com>
2025-01-20 01:54:21 +0000

    game files

---
+++ Clarke Games/Assets/Thoth/Cartoon UI Pack/Scripts/MyCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCursor : MonoBehaviour
+{
+    public Texture2D myCursor;          // Normal cursor texture
+    public Texture2D myCursorDark;      // Darker cursor texture for click state
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        // Set the default cursor at the start
+        Cursor.SetCursor(myCursor, hotSpot, CursorMode.Auto);
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        // Check for mouse click (left button)
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(myCursorDark, hotSpot, CursorMode.Auto);
+        }
+
+        // Check for mouse release
+        if (Input.GetMouseButtonUp(0))
+        {
+            Cursor.SetCursor(myCursor, hotSpot, CursorMode.Auto);
+        }
+    }
+}
---
+++ Clarke Games/Assets/textController.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class textController : MonoBehaviour
+{
+    [SerializeField] TMP_Text textbox;
+    [SerializeField] List<Dialog> dialog;
+
+    private int index = 0;
+    public bool CanGoNext;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        clear();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !CanGoNext)
+        {
+            CanGoNext= true;
+            UpdateLine(index);
+        }
+        else if (CanGoNext && Input.GetMouseButtonDown(0))
+        {
+            NextLine();
+        }
+        else if (CanGoNext && Input.GetMouseButtonDown(1))
+        {
+            PreviousLine();
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index >= dialog.Count)
+        {
+            return;
+        }
+        index++;
+        UpdateLine(index);
+
+    }
+    public void PreviousLine()
+    {
+        if (index <= 0)
+        {
+            return;
+        }
+        index--;
+        UpdateLine(index);
+
+    }
+
+    private void UpdateLine(int index)
+    {
+        textbox.text = dialog[index].text;
+    }
+
+    public void clear()
+    {
+        textbox.text = "";
+    }
+
+}
+
+[System.Serializable]
+public class Dialog
+{
+    public int index;
+    public string text;
+}
